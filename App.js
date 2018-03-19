1185f1cc3aeb1317d0ac69edcd0ea227791d5c26
thanawat <tha_chookes@hotmail.com>
2018-03-19 23:38:44 +0700

    handle menu

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Body
+} from 'native-base'
 import styled from 'styled-components'
-
+import { compose, withState } from 'recompose'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,25 +20,55 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu'
 })
 
-const Container = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #000;
-  height: 100%;
-`
+// const StyledContent= styled(Content)`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: #000;
+//   height: 100%;
+// `
 
 const TextTitle = styled.Text`
-  color: #FFF;
+  color: #000;
 `
 
-export default class App extends Component {
-  render() {
-    return (
-      <Container>
-        <TextTitle>{instructions}</TextTitle>
-      </Container>
-    )
-  }
+const ContentBody = styled(Content)`
+  background-color: #000;
+  display: flex;
+`
+
+const ContentText = styled.Text`
+  color: #fff;
+`
+
+const menu = [0, 1, 2]
+
+const App = props => {
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Text>Overview</Text>
+        </Body>
+      </Header>
+      <ContentBody>
+        <ContentText>{props.menuValue}</ContentText>
+      </ContentBody>
+      <Footer>
+        <FooterTab>
+          {menu.map((item, index) => (
+            <Button
+              key={index}
+              active={index === props.menuValue}
+              onPress={() => props.setMenuValue(index)}
+            >
+              <TextTitle>{item}</TextTitle>
+            </Button>
+          ))}
+        </FooterTab>
+      </Footer>
+    </Container>
+  )
 }
 
+export default compose(withState('menuValue', 'setMenuValue', 1))(App)
